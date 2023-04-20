0ed1c67d69ced137c5382f32049c26ef132f1654
David Costoya <davidmartincostoya@gmail.com>
2023-04-20 20:54:56 -0300

    first commit

---
+++ js/calculadora.js
@@ -0,0 +1,145 @@
+
+
+// array (const global)
+const OPERACIONES = ["+", "-", "*", "/"];
+const NOMBRE = localStorage.getItem("Nombre")
+
+let valorVisor = 0;
+let numeroA;
+let numeroB;
+let operacion;
+let ahora = new Date();
+
+//----------
+
+// Mostrar el nombre, saludo y reseteo al refrescar
+
+
+
+//poner en 0 el display al refrescar
+
+window.onload = inicio
+
+window.onload = function() {
+  swal({
+    title: "¡Bienvenido a mi calcu!!!",
+    text: "Calcule responsablemente!!",
+    button: "Aceptar",
+  });
+}
+
+function inicio() {
+  document.getElementById("visor").value = "";
+}
+
+//calculos
+
+function boton(tecla) {
+  let auxiliar = document.getElementById("visor").value;
+  document.getElementById("visor").value = auxiliar + tecla;
+  valorVisor = document.getElementById("visor").value = auxiliar + tecla;
+}
+
+function btn_suma(caracter) {
+  numeroA = valorVisor;
+  numeroB = valorVisor;
+  operacion = OPERACIONES[0];
+  limpar();
+}
+
+function btn_resta(caracter) {
+  numeroA = valorVisor;
+  numeroB = valorVisor;
+  operacion = OPERACIONES[1];
+  limpar();
+}
+
+function btn_multiplicacion(caracter) {
+  numeroA = valorVisor;
+  numeroB = valorVisor;
+  operacion = OPERACIONES[2];
+  limpar();
+}
+
+function btn_division(caracter) {
+  numeroA = valorVisor;
+  numeroB = valorVisor;
+  operacion = OPERACIONES[3];
+  limpar();
+}
+
+function reset() {
+  document.getElementById("visor").value = "";
+  valorVisor = 0;
+  operacion = "";
+}
+
+function limpar() {
+  document.getElementById("visor").value = "";
+}
+
+function btn_igual() {
+  numeroB = valorVisor;
+  resultado();
+}
+
+//conteo de resultados!
+
+let button = document.getElementById("boton=");
+let count = 0;
+
+for (let i = 0; i < 1; i++) {
+  button.addEventListener("click", function () {
+    count++;
+    if (count === 1) {
+      console.log("He calculado mi primer resultado!")
+    }
+    else {
+      console.log("Ya he calculado " + count + " resultados! Soy grossa!");
+    }
+  });
+}
+
+//resultados
+
+function resultado() {
+  let total = 0;
+  let ultimoTotal = 0;
+
+  if (operacion === "+") {
+    total = Number(numeroA) + Number(numeroB);
+  } else if (operacion === "-") {
+    total = Number(numeroA) - Number(numeroB);
+  } else if (operacion === "*") {
+    total = Number(numeroA) * Number(numeroB);
+  } else if (operacion === "/") {
+    total = Number(numeroA) / Number(numeroB);
+  }
+
+  ultimoTotal = total;
+  reset();
+  document.getElementById("visor").value = total;
+  valorVisor = ultimoTotal;
+}
+
+//boton para salir
+
+document.getElementById("btn_salir").onclick = function () {
+  swal({
+    title: "¿Estás seguro de que deseas salir de la aplicación?",
+    icon: "warning",
+    buttons: ["Cancelar", "Sí"],
+  }).then(function (confirmado) {
+    if (confirmado) {
+      location.href = "about:blank";
+    } else {
+      swal("¡Bien pensado !", {
+        icon: "success",
+      });
+      reset();
+    }
+  });
+}
+
+
+
---
+++ js/login.js
@@ -0,0 +1,62 @@
+const registro_button = document.querySelector(".button");
+registro_button.onclick = (e) => {
+	e.preventDefault();
+
+	//datos que se van a recibir
+	const nombre = document.getElementById("nombre").value;
+	const email = document.getElementById("email").value;
+	const contraseña = document.getElementById("contraseña").value;
+	const ccontraseña = document.getElementById("ccontraseña").value;
+
+	//crear objeto con los datos
+	const usuario = {
+		nombre,
+		email,
+		contraseña,
+		ccontraseña
+	};
+
+	//convertir objeto a cadena JSON
+	const usuarioJSON = JSON.stringify(usuario);
+
+	//guardar cadena JSON en el almacenamiento local
+	localStorage.setItem('usuario', usuarioJSON);
+
+	//condicionales
+	if(nombre == "" && email == "" && contraseña == "" && ccontraseña == ""){
+		swal("Advertencia!", "Debes completar todos los campos!", "error");
+	}
+	else
+	{
+		if(contraseña !== ccontraseña){
+			swal("Advertencia!", "Las contraseñas no coinciden!", "error");
+		}else{
+			swal("Usted está registrado!", "juguemos!", "success"),
+			window.location.replace("index.html");
+		}
+	}
+}
+
+//acceso una vez registrado
+const login = document.querySelector('.login');
+login.onclick = (e) => {
+    e.preventDefault();
+
+    //traer valores desde la pagina de inicio de sesion
+    const Direccionemail = document.getElementById("email").value;
+    const passWord = document.getElementById("contraseña").value;
+
+    //obtener cadena JSON desde el almacenamiento local y convertirla a objeto
+    const usuarioJSON = localStorage.getItem("usuario");
+    const usuario = JSON.parse(usuarioJSON);
+
+    if(Direccionemail == "" && passWord == ""){
+        swal("Advertencia!", "No completaste ningún campo", "error");
+    }else{
+        if(Direccionemail == usuario.email && passWord == usuario.contraseña){
+            window.location.replace("./paginas/calcu.html"); 
+        }else{
+            swal("Advertencia!", "Controla bien tus datos", "error");
+        }
+    }
+}
